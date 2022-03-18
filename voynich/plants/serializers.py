4b9de50f025d7fc3b8dc99c7c3f89d5f0f8b20e7
Moritz Esser <moritz_esser@hotmail.de>
2022-03-18 09:48:09 +0100

    Update Serializers

--- voynich/plants/serializers.py
+++ voynich/plants/serializers.py
@@ -2,18 +2,13 @@ from rest_framework import serializers
 from .models import Plant, Label, Person, Publication, Classification
 
 
-class PlantSerializer(serializers.ModelSerializer):
-    classifications = serializers.StringRelatedField(many=True,read_only=True)
-    class Meta:
-        model = Plant
-        fields = ['id','page','page_image','svg','classifications']
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = ('id','first_name','last_name')
 
-class PublicationSerializer(serializers.ModelSerializer):
+class PublicationSerializer(serializers.ListSerializer):
     class Meta:
         model = Publication
         fields = ('id','name','year')
@@ -23,3 +18,9 @@ class ClassificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Classification
         fields = '__all__'
+
+class PlantSerializer(serializers.ModelSerializer):
+    classifications = ClassificationSerializer(many=True)
+    class Meta:
+        model = Plant
+        fields = ['id','page','page_image','svg','classifications']
